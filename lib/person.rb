5fba2a75894e6fee0097556a822d563523c796ec
MishaHerscu <mishaherscu@gmail.com>
2016-06-09 14:36:47 -0400

    work during classtime

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,12 @@
 require 'pry'
 
+# class
 class Person
+  attr_accessor :given_name
+  attr_accessor :surname
+  attr_accessor :favorite_food
+  attr_accessor :catchphrase
+
   def initialize(given_name, surname, favorite_food, catchphrase)
     @given_name = given_name
     @surname = surname
@@ -8,7 +14,72 @@ class Person
     @catchphrase = catchphrase
   end
 
-  def say_catchphrase
-    p @catchphrase
+  # # given_name getter
+  # def given_name
+  #   @given_name
+  # end
+  #
+  # # given_name setter
+  # def given_name=(given_name)
+  #   @given_name = given_name
+  # end
+  #
+  # # surname getter
+  # def surname
+  #   @surname
+  # end
+  #
+  # # surname setter
+  # def surname=(surname)
+  #   @surname = surname
+  # end
+  #
+  # # favorite_food getter
+  # def favorite_food
+  #   @favorite_food
+  # end
+  #
+  # # favorite_food setter
+  # def favorite_food=(favorite_food)
+  #   @favorite_food = favorite_food
+  # end
+  #
+  # # catchphrase getter
+  # def catchphrase
+  #   @catchphrase
+  # end
+  #
+  # # catchphrase setter
+  # def catchphrase=(catchphrase)
+  #   @catchphrase = catchphrase
+  # end
+
+  def say_catchphrase(num)
+    phrase = @catchphrase
+    final_statement = phrase * num
+    p final_statement
   end
 end
+
+# class
+# class Outfits
+#   def initialize(tops, bottoms, shoes, flair)
+#     @tops = tops
+#     @bottoms = bottoms
+#     @shoes = shoes
+#     @flair = flair
+#   end
+#
+#   def fresh?
+#     @flair == true
+#   end
+# end
+
+# experimental class factory
+# def class(name, instance_variable_names)
+#   class name
+#     for i in 0.upto(instance_variable_names.length)
+#       attr_accessor :instance_variable_names[i]
+#     end
+#   end
+# end
