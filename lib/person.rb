e0e43a6a0339aa78e0f214f6d4e7627a9d9a7416
MishaHerscu <mishaherscu@gmail.com>
2016-06-09 10:11:28 -0400

    person

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
 require 'pry'
+
+class Person
+  def initialize(given_name, surname, favorite_food, catchphrase)
+    @given_name = given_name
+    @surname = surname
+    @favorite_food = favorite_food
+    @catchphrase = catchphrase
+  end
+
+  def say_catchphrase
+    p @catchphrase
+  end
+end
